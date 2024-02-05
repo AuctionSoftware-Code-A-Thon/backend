4bc9671a28d7b4555c02fcbf964f69a950853d83
Buddi Navaneeth Kumar <35196622+Nava308@users.noreply.github.com>
2024-02-04 19:25:34 -0600

    created server, service and constants

---
+++ src/helpers/constants.ts
@@ -0,0 +1,23 @@
+// Application constants
+export const PORT: number = 8080;
+export const CLIENT_PORT: number = 3000;
+export const SESSION_TOKEN_COOKIE: string = "BACKEND-AUTH-AUTION-SOFTWARE";
+export const DOMAIN: string = "localhost";
+export const IDENTITY = "identity";
+
+//Success Messages
+export const LOG_OUT_SUCCESS = "Successfully loggedout";
+
+// Error Messages
+export const INVALID_SESSION: string =
+  "Session token not found, Please login to complete this transaction";
+export const INVALID_LOGIN_DETAILS: string =
+  "Login details provided are invalid";
+export const USER_NOT_FOUND: string = "User does not exist";
+export const WRONG_PASSWORD: string = "Wrong password";
+export const INVALID_USER_DETAILS: string = "User details provided are invalid";
+export const DUPLICATE_USER: string = "User already exist";
+export const INVALID_REQUEST: string = "Invalid request";
+export const SYSTEM_ERROR: string = "System error occured";
+export const SOMETHING_WENT_WRONG: string =
+  "Something went wrong, Please contact admin";
---
+++ src/services/authentication.ts
@@ -0,0 +1,102 @@
+import express from "express";
+import { User } from "helpers/types";
+import { get } from "lodash";
+import { createUser, getUserByEmail, updateUser } from "../db/user/user";
+import { authenticate, random } from "../helpers";
+import {
+  DOMAIN,
+  DUPLICATE_USER,
+  INVALID_LOGIN_DETAILS,
+  INVALID_USER_DETAILS,
+  LOG_OUT_SUCCESS,
+  SESSION_TOKEN_COOKIE,
+  SYSTEM_ERROR,
+  USER_NOT_FOUND,
+  WRONG_PASSWORD,
+} from "../helpers/constants";
+import {
+  validateCreateUserData,
+  validateLoginData,
+} from "../helpers/validations";
+export const login = async (req: express.Request, res: express.Response) => {
+  try {
+    const valid = validateLoginData(req.body);
+    if (valid.error) {
+      return res.status(400).json({ errorMessage: INVALID_LOGIN_DETAILS });
+    }
+
+    const user = await getUserByEmail(valid.value.email);
+
+    if (!user) {
+      return res.status(400).json({ errorMessage: USER_NOT_FOUND });
+    }
+
+    const expectedHash = authenticate(user.salt, valid.value.password);
+
+    if (user.saltedPassword != expectedHash) {
+      return res.status(403).json({ errorMessage: WRONG_PASSWORD });
+    }
+
+    const salt = random();
+    user.sessionToken = authenticate(salt, user.email);
+
+    const updatedUser = await updateUser(user);
+
+    res.cookie(SESSION_TOKEN_COOKIE, user.sessionToken, {
+      domain: DOMAIN,
+      path: "/",
+      httpOnly: true,
+    });
+
+    return res.status(200).json(updatedUser).end();
+  } catch (error) {
+    return res.status(500).json({
+      errorMessage: SYSTEM_ERROR,
+      systemError: error,
+    });
+  }
+};
+export const logout = async (req: express.Request, res: express.Response) => {
+  try {
+    const user = get(req, "identity") as User;
+    user.sessionToken = "";
+    await updateUser(user);
+    return res.status(200).json({
+      successMessage: LOG_OUT_SUCCESS,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      errorMessage: SYSTEM_ERROR,
+      systemError: error,
+    });
+  }
+};
+export const register = async (req: express.Request, res: express.Response) => {
+  try {
+    const valid = validateCreateUserData(req.body);
+    if (valid.error) {
+      return res.status(400).json({ errorMessage: INVALID_USER_DETAILS });
+    }
+    const existingUser = await getUserByEmail(req.body.email);
+    if (existingUser) {
+      return res.status(400).json({ errorMessage: DUPLICATE_USER });
+    }
+    const salt = random();
+    const user = await createUser({
+      email: valid.value.email,
+      saltedPassword: authenticate(salt, valid.value.password),
+      address: valid.value.address,
+      firstName: valid.value.firstName,
+      lastName: valid.value.lastName,
+      phoneNumber: valid.value.phoneNumber,
+      sessionToken: authenticate(salt, valid.value.email),
+      salt: salt,
+    });
+    return res.status(200).json(user).end();
+  } catch (error) {
+    return res.status(500).json({
+      errorMessage: SYSTEM_ERROR,
+      systemError: error,
+    });
+  }
+};
