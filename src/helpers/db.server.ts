c248028bb90e7e181fce9c7a898c3fe524c60502
Buddi Navaneeth Kumar <35196622+Nava308@users.noreply.github.com>
2024-02-04 19:26:09 -0600

    singleton db instance

---
+++ src/helpers/db.server.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+
+let db: PrismaClient;
+
+declare global {
+  var _db: PrismaClient | undefined;
+}
+
+if (!global._db) {
+  global._db = new PrismaClient();
+}
+
+db = global._db;
+
+export { db };
